51e0ea4cda8560300f0ff954c16d55938f77087b
Larissa <larissaolaguiar@gmail.com>
2024-12-12 12:56:52 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr(fatura, pecas) {
-
+  function getPeca(apresentacao) {
+    return pecas[apresentacao.id];
+  }
   // Função extraída
-  function calcularTotalApresentacao(apre, peca) {
+  function calcularTotalApresentacao(apre) {
       let total = 0;
-      switch (peca.tipo) {
+      switch (getPeca(apre).tipo) {
           case "tragedia":
               total = 40000;
               if (apre.audiencia > 30) {
@@ -33,16 +35,16 @@ function gerarFaturaStr(fatura, pecas) {
                           minimumFractionDigits: 2 }).format;
 
   for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = calcularTotalApresentacao(apre, peca);
+      
+      let total = calcularTotalApresentacao(apre);
 
       // Créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
           creditos += Math.floor(apre.audiencia / 5);
 
       // Mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
   }
 
